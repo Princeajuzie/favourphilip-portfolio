b7a0d711f4122503e4e7c67112bf1c93aef38e73
princeajuzie7 <princeajuzie7@gmail.com>
2024-01-24 19:05:50 +0100

    feat:error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { Roboto } from "next/font/google";
-import { Layout, FixedPlugin } from "@/components";
+import { Layout } from "@/components";
 
 const roboto = Roboto({
   subsets: ["latin"],
