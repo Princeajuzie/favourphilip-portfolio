a4caf4d85d40526ed56fcb701c704595120c11d3
princeajuzie7 <princeajuzie7@gmail.com>
2024-01-25 14:24:09 +0100

    fix: changes on the hero section

--- src/app/hero.tsx
+++ src/app/hero.tsx
@@ -13,57 +13,20 @@ function Hero() {
         alt="bg-img"
         className="absolute inset-0 ml-auto w-[920px] h-[780px] rounded-bl-[100px] object-cover object-center"
       />
-      <div className="container mx-auto mt-28">
-        <div className="grid grid-cols-12  text-center lg:text-left">
-          <Card className="col-span-full rounded-xl border border-white bg-white/90 py-10 p-8 shadow-lg shadow-black/10 backdrop-blur-sm backdrop-saturate-200 xl:col-span-7">
-            <Typography
-              variant="h1"
-              color="blue-gray"
-              className="lg:text-5xl !leading-snug text-3xl lg:max-w-3xl"
-            >
-              Unlock the Power of the Web with Our Expert Courses
-            </Typography>
+      <div className=" md:w-fit h-fit  mx-auto   m-auto">
+        <div className="grid grid-cols-12  text-center lg:text-left ">
+          <Card className="col-span-full rounded-xl border border-white bg-white/90 px-5 shadow-lg shadow-black/10 backdrop-blur-sm backdrop-saturate-200 xl:col-span-7 border-3 " >
+         
             <Typography variant="lead" className="mb-10 mt-6 !text-gray-900">
-              Are you ready to embark on an exciting journey into the world of
-              web development? Look no further! We are your trusted partner for
-              mastering the art of web development.
+            How to make $5,000+ Per Month Selling Products on Amazon as a Nigerian.
             </Typography>
-            <div className="mb-8 flex justify-center gap-4 lg:justify-start">
+            <div className="mb-8 flex justify-center gap-4 lg:justify-start flex-wrap">
               <Button color="gray">view all courses</Button>
               <Button color="gray" variant="outlined">
                 see pricing
               </Button>
             </div>
-            <div className="grid grid-cols-2 lg:grid-cols-4 items-center justify-between gap-4 lg:justify-start">
-              <Image
-                width={144}
-                height={144}
-                className="w-36 grayscale opacity-60"
-                src="/logos/logo-pinterest.svg"
-                alt="pinterest"
-              />
-              <Image
-                width={144}
-                height={144}
-                className="w-36 grayscale opacity-60"
-                src="/logos/logo-netflix.svg"
-                alt="netflix"
-              />
-              <Image
-                width={144}
-                height={144}
-                className="w-36 grayscale opacity-60"
-                src="/logos/logo-coinbase.svg"
-                alt="coinbase"
-              />
-              <Image
-                width={144}
-                height={144}
-                className="w-36 grayscale opacity-60"
-                src="/logos/logo-google.svg"
-                alt="google"
-              />
-            </div>
+        
           </Card>
         </div>
       </div>
