063d5016b278730d4172efb9374747c09e569d73
princeajuzie7 <princeajuzie03@gmail.com>
2024-01-27 12:47:26 +0100

    feat: bounce animation added

--- src/app/explore-courses.tsx
+++ src/app/explore-courses.tsx
@@ -72,7 +72,35 @@ export function ExploreCourses() {
   </g>
 </svg>
 
-                 Click here to get it now  </Button>
+                 Click here to get it now  
+                 
+
+
+
+<svg
+  width="34px"
+  height="34px"
+  viewBox="0 0 24 24"
+  fill="none"
+  xmlns="http://www.w3.org/2000/svg"
+  className="animate-bounce"
+>
+  <g id="SVGRepo_bgCarrier" strokeWidth={0} />
+  <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round" />
+  <g id="SVGRepo_iconCarrier">
+    {" "}
+    <path
+      d="M9 3.5V2M5.06066 5.06066L4 4M5.06066 13L4 14.0607M13 5.06066L14.0607 4M3.5 9H2M8.5 8.5L12.6111 21.2778L15.5 18.3889L19.1111 22L22 19.1111L18.3889 15.5L21.2778 12.6111L8.5 8.5Z"
+      stroke="#FF0000"
+      strokeWidth={2}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />{" "}
+  </g>
+</svg>
+                 </Button>
+
+
               </Link>
       </div>
     </section>
