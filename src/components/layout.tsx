35911c66770ee9e2f5a0359ebc887e98f69e4503
princeajuzie7 <princeajuzie03@gmail.com>
2024-01-27 08:00:27 +0100

    feat : vercel analytics added

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,10 +1,11 @@
 "use client";
 
 import React from "react";
+import { Analytics } from '@vercel/analytics/react';
 import { ThemeProvider } from "@material-tailwind/react";
 
 export function Layout({ children }: { children: React.ReactNode }) {
-  return <ThemeProvider>{children}</ThemeProvider>;
+  return <ThemeProvider>{children} <Analytics /></ThemeProvider>;
 }
 
 export default Layout;
